8ad96eee2c8d231df755a236985400c0aa264627
CharCoderoid <lexakuo@gmail.com>
2026-02-28 15:31:23 -0800

    Added login components

--- FriendZone/src/login.jsx
+++ FriendZone/src/login.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-import Login from "./components/login.jsx"
+import Login from "./components/loginpage.jsx"
 import Signup from "./components/signup.jsx"
 
 export default function login() {
     return (
+
         <div>
             <h1>Welcome to FriendZone</h1>
             <h2>Login</h2>
+
         </div>
     )
 }
